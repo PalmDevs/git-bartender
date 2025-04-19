02dcefd14105f7009a6387f9b49354ac47416dda
PalmDevs <git@palmdevs.me>
2025-04-19 14:39:51 +0700

    fix(context/logger): only print out debug logs when env is set

--- src/context/logger.ts
+++ src/context/logger.ts
@@ -14,10 +14,12 @@ export const logger = {
     error: (...args: any[]) =>
         console.error(chalkTemplate`{${string('product.color.primary')} ${figures.cross} ${loggablesToString(args)}}`),
     warn: (...args: any[]) => console.warn(chalkTemplate`{yellowBright ${figures.warning} ${loggablesToString(args)}}`),
-    debug: (...args: any[]) =>
-        console.debug(
-            chalkTemplate`{${string('product.color.tertiary')} ${figures.pointer} ${loggablesToString(args)}}`,
-        ),
+    debug: (...args: any[]) => {
+        if (process.env.GB_DEBUG)
+            console.debug(
+                chalkTemplate`{${string('product.color.tertiary')} ${figures.pointer} ${loggablesToString(args)}}`,
+            )
+    },
     log: (...args: any[]) => console.log(chalkTemplate`${loggablesToString(args)}`),
     success: (...args: any[]) => console.info(chalkTemplate`{greenBright ${figures.tick} ${loggablesToString(args)}}`),
     newline: () => console.log(''),
