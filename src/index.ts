d64dddfdcdeefab489c2c956f7d51a309960dd40
PalmDevs <git@palmdevs.me>
2025-04-19 14:40:14 +0700

    feat: add debug logs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { execute as executeUnknown } from './commands/[unknown]'
 import { execute as executeHelp } from './commands/help'
-import { args, clearArgs, clearFlags, command, flags } from './context'
+import { args, clearArgs, clearFlags, command, flags, logger } from './context'
 import { string } from './strings'
 import { tryResolveCommand } from './utils'
 
@@ -9,10 +9,17 @@ process.title = string('product.name')
 let ignorable: string
 
 try {
+    logger.debug(`Command: ${command}, Args: ${JSON.stringify(args)}, Flags: ${JSON.stringify(flags)}`)
+
     const { name: cmdName, cmd } = tryResolveCommand(command)
     if (!cmd) throw (ignorable = 'Command not found')
 
+    logger.debug(`Resolved command: ${cmdName}`)
+    logger.debug('Command object:', cmd)
+
     if ('h' in flags || 'help' in flags) {
+        logger.debug('User requested help via flags')
+
         clearArgs()
         clearFlags()
         args.push(cmdName)
@@ -24,6 +31,10 @@ try {
         await execute()
     }
 } catch (e) {
+    logger.debug(`Caught error (ignorable = ${Boolean(ignorable!)}):`, e)
     if (ignorable!) await executeUnknown()
-    else throw e
+    else {
+        logger.error(string('generic.error'))
+        console.error(e)
+    }
 }
