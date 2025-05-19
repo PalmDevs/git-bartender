e8a49ada9bbf534b49d478ec6a348fbe44e1060c
madkarmaa <madkarma99@gmail.com>
2025-05-19 10:40:18 +0000

    refactor: simplify random branch name generation

--- src/commands/delete/history.ts
+++ src/commands/delete/history.ts
@@ -1,18 +1,7 @@
-import { $ } from 'bun'
+import { $, randomUUIDv7 } from 'bun'
 import { logger } from '../../context'
 import { string } from '../../strings'
 import { getActiveBranch, getBranchRemote } from '../../utils/git'
-
-const randomId = (length = 10): string => {
-    let result = ''
-    const digits = '0123456789'
-
-    for (let i = 0; i < length; i++) {
-        result += digits.charAt(Math.floor(Math.random() * digits.length))
-    }
-
-    return result
-}
 
 export const execute = async () => {
     const oldBranch = await getActiveBranch()
@@ -20,7 +9,7 @@ export const execute = async () => {
 
     const remote = await getBranchRemote(oldBranch)
 
-    await $`git checkout --orphan ${randomId(16)}`.quiet()
+    await $`git checkout --orphan ${randomUUIDv7()}`.quiet()
     await $`git add .`.quiet()
     await $`git commit -m "chore: init"`.quiet()
     await $`git branch -D ${oldBranch}`.quiet()
